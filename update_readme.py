fb641095e3b06c9d84c396fb268eae6789612c7d
xdearboy <105206420+xdearboy@users.noreply.github.com>
2025-01-21 15:50:38 +0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -16,12 +16,15 @@ def update_readme(repos):
     start_index = content.index(START_MARKER) + 1
     end_index = content.index(END_MARKER)
 
+    header = "| Префикс                  | Язык программирования | Описание                                                 |\n"
+    separator = "|--------------------------|-----------------------|---------------------------------------------------------|\n"
+    
     repo_lines = [
         f"| {repo['name']} | {repo.get('language', 'Unknown')} | {repo.get('description', 'No description')} |\n"
         for repo in repos
     ]
     
-    content = content[:start_index] + repo_lines + content[end_index:]
+    content = content[:start_index] + [header, separator] + repo_lines + content[end_index:]
 
     with open(README_FILE, "w") as file:
         file.writelines(content)
